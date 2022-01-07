c65855cd80b8f0d1fff8d3b0a9b7d9d89b3e77ba
SharmanJ <kirtisjeurkar@gmail.com>
2022-01-07 15:12:14 +0530

    project set up with hello world html page

---
+++ src/main/java/com/sharman/ChemistryCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.sharman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemistryCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemistryCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sharman/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.sharman.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController 
+{
+	@RequestMapping("/")
+	public String callHome() 
+	{
+		return "home";
+	}
+	
+}
