f4082018b0144d1c0d22675d9288702c5f68d687
SharmanJ <kirtisjeurkar@gmail.com>
2022-01-19 12:03:34 +0530

    Function added for parameters requesting for further process

--- src/main/java/com/sharman/controller/HomeController.java
+++ src/main/java/com/sharman/controller/HomeController.java
@@ -1,42 +1,41 @@
 package com.sharman.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/Chemistry-Calculator/")
-public class HomeController 
-{
+public class HomeController {
 	@RequestMapping("/")
-	public String callHome() 
-	{
+	public String callHome() {
 		return "home";
 	}
-	
+
 	@RequestMapping("/about")
-	public String callAbout() 
-	{
+	public String callAbout() {
 		return "about";
 	}
-	
+
 	@RequestMapping("/Mole-Calculator")
-	public String callMoleCalculator() 
-	{
+	public String callMoleCalculator() {
 		return "Mole-Calculator";
 	}
-	
-	@RequestMapping("Number-Of-Moles")
-	public String calculateMoles()
-	{
-		return "Mole-Calculator";	
-	}
-	
+
+
+	  @GetMapping("/Number-Of-Moles") 
+	  public String calculateMoles(@RequestParam(value="givenMass")String
+	  givenMass,@RequestParam(value="molarMass")String molarMass) { 
+ 
+	  return "Mole-Calculator"; 
+	  }
+	 
+
 	@RequestMapping("/Avagadros-Number")
-	public String callAvagadrosNumber() 
-	{
+	public String callAvagadrosNumber() {
 		return "Avagadros-Number";
 	}
-	
-	
+
 }
