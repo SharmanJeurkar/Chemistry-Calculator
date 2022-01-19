19cbd03c1a28057fb91e06e0fd71bcffe21d0b80
SharmanJ <kirtisjeurkar@gmail.com>
2022-01-19 15:55:17 +0530

    Added Service Class

---
+++ src/test/java/com/sharman/ChemistryCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemistryCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
