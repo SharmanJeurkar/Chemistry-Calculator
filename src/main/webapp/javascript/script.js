56e39288009e62a6f226bde3a715742109e157a0
SharmanJ <kirtisjeurkar@gmail.com>
2022-01-22 10:21:29 +0530

    added onclick function for links which are going to be used in dropdown. Working on #5 and #6.

---
+++ src/main/webapp/javascript/script.js
@@ -0,0 +1,14 @@
+function readonlyForGivenMass() {
+  var x = document.getElementById("givenMass").readOnly;
+  
+}
+
+function readonlyForMolarMass() {
+  var x = document.getElementById("molarMass").readOnly;
+  
+}
+
+function readonlyForMoles() {
+  var x = document.getElementById("moles").readOnly;
+  
+}
