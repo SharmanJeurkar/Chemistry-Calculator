b984a2ca04e5ceccbad827a2691fe3f298750fd9
SharmanJ <kirtisjeurkar@gmail.com>
2022-01-24 12:56:23 +0530

    added functions for calculating Molar Mass and Given Mass. Working #7

--- src/main/java/com/sharman/service/Calculator.java
+++ src/main/java/com/sharman/service/Calculator.java
@@ -7,6 +7,16 @@ public class Calculator {
 		return moles;
 	}
 	
+	public double calculateMolarMass(double givenMass, double moles) {
+		double molarMass = givenMass/moles;
+		return molarMass;
+	}
+	
+	public double calculateGivenMass(double molarMass, double moles) {
+		double givenMass = molarMass*moles;
+		return givenMass;
+	}
+	
 	public double calculateAvagadrosNumber(double moles) {
 		
 		double result= 6.025*moles;
