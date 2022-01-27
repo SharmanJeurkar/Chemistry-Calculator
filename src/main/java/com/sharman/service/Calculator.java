9e904457794d38c617da1c84756f10414a262431
SharmanJ <kirtisjeurkar@gmail.com>
2022-01-27 10:08:55 +0530

    due to data loss, recommitted some data.

--- src/main/java/com/sharman/service/Calculator.java
+++ src/main/java/com/sharman/service/Calculator.java
@@ -7,9 +7,19 @@ public class Calculator {
 		return moles;
 	}
 	
+	public double calculateMolarMass(double givenMass, double moles) {
+		double molarMass = givenMass/moles;
+		return molarMass;
+	}
+
+	public double calculateGivenMass(double molarMass, double moles) {
+		double givenMass = molarMass*moles;
+		return givenMass;
+	}
+	
 	public double calculateAvagadrosNumber(double moles) {
 		
-		double result= 6.025*moles;
+		double result= 6.022*moles;
 		return result;
 	}
 	
