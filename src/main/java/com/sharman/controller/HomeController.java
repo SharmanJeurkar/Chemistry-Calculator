00a3aed82c9d8bd4ed14dff91da9453df1d930ab
SharmanJeurkar <kirtisjeurkar@gmail.com>
2022-02-04 10:54:25 +0530

    added controller for redirecting the new button in mole-calculator.jsp

--- src/main/java/com/sharman/controller/HomeController.java
+++ src/main/java/com/sharman/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.sharman.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,34 +26,35 @@ public class HomeController {
 		return "Mole-Calculator";
 	}
 
-	@RequestMapping("/Number-Of-Moles")
-	public String calculateMoles(@RequestParam(value = "givenMass",  required=false) Double givenMassInput,
-								 @RequestParam(value = "molarMass",  required=false) Double molarMassInput,
-								 @RequestParam(value = "moles",  required=false) Double molesInput, Model model) {
-
-		Calculator calculate = new Calculator();
-
-		if (molesInput == null) {
-			double moles = calculate.calculateMoles(givenMassInput, molarMassInput);
-			model.addAttribute("givenMass", givenMassInput);
-			model.addAttribute("molarMass", molarMassInput);
-			model.addAttribute("moles", moles);
-
-		}else if (givenMassInput == null) {
-			double givenMass = calculate.calculateGivenMass(molarMassInput, molesInput);
-			model.addAttribute("molarMass", molarMassInput);
-			model.addAttribute("moles", molesInput);
-			model.addAttribute("givenMass", givenMass);
-		}else if (molarMassInput == null) {
-			double molarMass = calculate.calculateMolarMass(givenMassInput, molesInput);
-			model.addAttribute("givenMass", givenMassInput);
-			model.addAttribute("moles", molesInput);
-			model.addAttribute("molarMass", molarMass);
-		}
-
-		return "Mole-Calculator";
-	}
-
+	
+	  @RequestMapping("/Number-Of-Moles") public String
+	  calculateMoles(@RequestParam(value = "givenMass", required=false) Double
+	  givenMassInput,
+	  
+	  @RequestParam(value = "molarMass", required=false) Double molarMassInput,
+	  
+	  @RequestParam(value = "moles", required=false) Double molesInput, Model
+	  model) {
+	  
+	  Calculator calculate = new Calculator();
+	  
+	  if (molesInput == null) { double moles =
+	  calculate.calculateMoles(givenMassInput, molarMassInput);
+	  model.addAttribute("givenMass", givenMassInput);
+	  model.addAttribute("molarMass", molarMassInput); model.addAttribute("moles",
+	  moles);
+	  
+	  }else if (givenMassInput == null) { double givenMass =
+	  calculate.calculateGivenMass(molarMassInput, molesInput);
+	  model.addAttribute("molarMass", molarMassInput); model.addAttribute("moles",
+	  molesInput); model.addAttribute("givenMass", givenMass); }else if
+	  (molarMassInput == null) { double molarMass =
+	  calculate.calculateMolarMass(givenMassInput, molesInput);
+	  model.addAttribute("givenMass", givenMassInput); model.addAttribute("moles",
+	  molesInput); model.addAttribute("molarMass", molarMass); }
+	  
+	  return "Mole-Calculator"; }
+	 
 	@RequestMapping("/Avagadros-Number")
 	public String callAvagadrosNumber() {
 		return "Avagadros-Number";
@@ -67,6 +69,11 @@ public class HomeController {
 		System.out.println(power);
 		model.addAttribute("result", result + "x" + power);
 		model.addAttribute("moles", moles);
+		return "Avagadros-Number";
+	}
+	
+	@PostMapping("/Calculate-Avogadros-With-Moles")
+	public String btn2() {
 		return "Avagadros-Number";
 	}
 
